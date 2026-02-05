ee0afa97ac35b040005d06528ccd091a32fef099
DivyaS <127476291+Sdivya15@users.noreply.github.com>
2026-02-05 06:36:32 +0530

    Add small example of Characters

---
+++ src/main/java/org/DataTypes/Char.java
@@ -0,0 +1,12 @@
+package org.DataTypes;
+
+public class Char {
+
+
+    public static void main(String[] args) {
+        char a = 'A';
+        System.out.println("Value of char a: "+ a);
+
+        System.out.println("Value of Int a: " + (int) a);//changes into Unicode values
+    }
+}
